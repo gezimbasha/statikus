4105aa9e0cc44f5110270c7421226b3d093dee87
Gëzim Basha <gezimbasha@outlook.com>
2013-05-05 23:29:57 +0200

    Modifkim baze i klases Pika.
    Signed-off-by: Gëzim Basha <gezimbasha@outlook.com>

---
+++ Pika.cpp
@@ -0,0 +1,11 @@
+#include "Pika.h"
+
+
+Pika::Pika(void)
+{
+}
+
+
+Pika::~Pika(void)
+{
+}
---
+++ Pika.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <Eigen\Eigen\Dense>
+using namespace Eigen;
+using namespace std;
+
+class Pika
+{
+public:
+	Pika(void);
+	~Pika(void);
+
+
+};
+
